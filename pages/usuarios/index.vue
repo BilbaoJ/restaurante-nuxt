6840fa6e1cc7d35eaa28e27397541bc682b40ab8
BilbaoJ <jessicabilbao02@gmail.com>
2022-05-07 23:11:00 -0500

    -CRUD version 1

--- pages/usuarios/index.vue
+++ pages/usuarios/index.vue
@@ -2,7 +2,7 @@
   <b-container>
     <h2>USUARIO</h2>
     <!-- Creación de formulario -->
-    <b-form @submit="onSubmit" @reset="onReset">
+    <b-form @submit="createUser" @reset="resetForm">
       <b-form-group
         id="input-group-1"
         label="Email:"
@@ -35,27 +35,47 @@
           required
         ></b-form-select>
       </b-form-group>
-
-
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+      <br />
+      <!-- Actions -->
+      <b-button type="submit" variant="primary" v-if="!editing"
+        >Crear Usuario</b-button
+      >
+      <b-button variant="success" @click="updateUser" v-else
+        >Guardar Usuario</b-button
+      >
+      <b-button type="reset" variant="danger">Limpiar Formulario</b-button>
     </b-form>
 
-    <b-table striped hover :items="users"></b-table>
+    <b-table striped hover :items="users" :fields="headers">
+      <template #cell(show_details)="row">
+        <b-button size="sm" @click="loadUser(row)" class="mr-2">
+          Modificar usuario
+        </b-button>
+
+        <b-button size="sm" @click="deleteUser(row)" class="mr-2">
+          Eliminar
+        </b-button>
+      </template>
+    </b-table>
   </b-container>
 </template>
 <script>
 export default {
+    // Información a utilizar
   data() {
     return {
+      headers: ["correo", "nombre", "universidad", "show_details"],
       users: [],
-      user:{},
-      universidades:["Universidad de Medellín", "Universidad de Antioquia"]
+      user: {},
+      universidades: ["Universidad de Medellín", "Universidad de Antioquia"],
+      editing: false,
     };
   },
+  // Método antes de que cargue la página
   beforeMount() {
     this.loadUsers();
   },
+  //Métodos a usar
   methods: {
     async loadUsers() {
       //Cargar usuarios de la base de datos
@@ -69,7 +89,7 @@ export default {
         alert("No se cargaron los usuarios");
       }
     },
-    async onSubmit(event) {
+    async createUser(event) {
       event.preventDefault();
 
       const url = "http://localhost:3001/api/v1/usuarios";
@@ -77,18 +97,28 @@ export default {
       console.log(data);
       this.loadUsers();
     },
-    onReset(event) {
+    async updateUser(event) {
       event.preventDefault();
+
+      const url = `http://localhost:3001/api/v1/usuarios/${this.user._id}`;
+      let { data } = await this.$axios.put(url, this.user);
+      console.log(data);
+      this.resetForm();
+      this.loadUsers();
+    },
+    loadUser(user) {
+      this.user = Object.assign({}, user.item);
+      this.editing = true;
+    },
+    async deleteUser({ item }) {
+      const url = `http://localhost:3001/api/v1/usuarios/${this.item._id}`;
+      let { data } = await this.$axios.delete(url);
+      this.loadUsers();
+    },
+    resetForm() {
       // Reset our form values
-      this.form.email = "";
-      this.form.name = "";
-      this.form.food = null;
-      this.form.checked = [];
-      // Trick to reset/clear native browser form validation state
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
+      this.editing = false;
+      this.user = {};
     },
   },
 };
